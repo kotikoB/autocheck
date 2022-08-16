5d8641a4faa133647b194be453e5b257f2ca5e4a
Stephen Ouma <stephenouma@SOuma.local>
2022-08-16 18:39:59 +0300

    updates

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -11,13 +11,14 @@ export class ArticlesService {
   fetchStory(id: number): Observable<Story> {
     const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
     const story = this.http.get(url);
-
     return story.pipe(
       map((x) => {
         return {
           id: x.data.id,
           type: x.data.type,
           by: x.data.by,
+          time: x.data.time,
+          title: x.data.title,
         };
       }),
     );
@@ -38,11 +39,18 @@ export class ArticlesService {
       }),
       map((list) => {
         list.forEach((x) => {
+          let words = [];
           x.subscribe({
             next(value) {
-              console.log(value);
+              // Most occuring word in title
+              let split = value.title.split(' ');
+              words = [...split];
+              words.push(value.title);
+              console.log('wawa', words);
+              console.log('mane', value);
             },
           });
+          console.log('masese', words);
           return x;
         });
         return list;
